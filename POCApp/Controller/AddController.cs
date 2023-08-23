8476d592641996cc2e11ab590715884b5e8fcba6
Prashant Vaddoriya <ex_vprashant@harsco.com>
2023-08-23 11:53:52 +0530

    Add project files.

---
+++ POCApp/Controller/AddController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace POCApp.Controller
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AddController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> Post([FromBody] Numbers numbers)
+        {
+            int sum = numbers.Num1 + numbers.Num2;
+            return sum;
+        }
+    }
+
+    public class Numbers
+    {
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+    }
+}
