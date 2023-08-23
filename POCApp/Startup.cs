faa814ba9a8b9f93f841e5df0edf1dd051adc47b
Prashant Vaddoriya <ex_vprashant@harsco.com>
2023-08-23 12:11:56 +0530

    Front End code added and host at IIS.

--- POCApp/Startup.cs
+++ POCApp/Startup.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -33,6 +34,7 @@ namespace POCApp
                     Description = "Add API"
                 });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +65,10 @@ namespace POCApp
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapDefaultControllerRoute();
+            });
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
             });
         }
     }
